ad3ab3a0db0ba9d5962aa2256dbb4240464b43ba
Amilcar Munoz <yellowballpython@gmail.com>
2023-03-09 02:20:35 -0600

    fix next config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
   },
 }
 
